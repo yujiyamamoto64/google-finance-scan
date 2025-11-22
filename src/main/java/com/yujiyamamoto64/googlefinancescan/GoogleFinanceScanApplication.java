b6c3c0c5c2ce39b93a53a937cac6f0ba162f65e7
Yuji Yamamoto <yujiyamamoto64@gmail.com>
2025-11-22 11:35:19 -0300

    first commit

---
+++ src/main/java/com/yujiyamamoto64/googlefinancescan/GoogleFinanceScanApplication.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.googlefinancescan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleFinanceScanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleFinanceScanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yujiyamamoto64/googlefinancescan/GoogleFinanceScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yujiyamamoto64.googlefinancescan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleFinanceScanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
